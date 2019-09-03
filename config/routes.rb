67dc7470086f47e667beba32ffdc7d9d48db6e7f
Irene Fernandes <fernandesm.irene@gmail.com>
2019-09-03 14:57:53 +0100

    added some controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'orders/index,'
+  get 'orders/show,'
+  get 'orders/create,'
+  get 'orders/new'
+  get 'ingredients/index'
+  get 'categories/index'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
