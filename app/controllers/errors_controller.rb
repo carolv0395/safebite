657196a17e6b50799a16427ea52a502ef5e6d22d
Pedro Pinheiro <pedrofgpinheiro@icloud.com>
2019-09-04 12:09:38 +0100

    pagina de erro

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,15 @@
+class ErrorsController < ApplicationController
+ skip_before_action :authenticate_user!, only: [:not_found,:internal_server_error]
+ skip_after_action :verify_authorized, only: [:not_found,:internal_server_error]
+
+
+
+
+  def not_found
+ 	 render(:status => 404)
+  end
+
+  def internal_server_error
+  	 render(:status => 500)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   devise_for :users
-  root to: 'products#index'
+  root to: 'pages#home'
+
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
+
 
   resources :allergens, except: :show do
     collection do
