1e6d66a92d92dfcd4f08614bd4fa5553efd39909
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-09-05 19:39:04 +0100

    cool JS magic

--- app/assets/javascripts/family_choice.js
+++ app/assets/javascripts/family_choice.js
@@ -1,10 +1,19 @@
-document.querySelector()
+const buttons = document.querySelectorAll(".family-choice");
+const icons = document.querySelectorAll(".hidden-icons");
+const checkboxes = document.querySelectorAll('input[type="checkbox"]')
 
-(document).ready(function(){
-  (".family-choice").click(function(){
-    (this).toggleClass("active");
-  });
-  (".family-choice img").click(function(){
-    (this).hidden = true;
+buttons.forEach((button) => {
+  const index = button.classList[1];
+  if (checkboxes[index].checked == true) {
+    button.classList.add("active");
+    icons[index].classList.remove('hidden-icons');
+  } else {
+    button.classList.remove("active");
+    icons[index].classList.add('hidden-icons');
+  }
+  button.addEventListener("click", function() {
+    button.classList.toggle("active");
+    const index = button.classList[1];
+    icons[index].classList.toggle('hidden-icons');
   });
 });
