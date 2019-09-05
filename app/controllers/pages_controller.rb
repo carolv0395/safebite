d1e37d4d4339eb671f8c2ede52c2c404f3fbe3c9
Antonio Boavida <antonioruela@gmail.com>
2019-09-05 16:19:55 +0200

    Styling for homepage and login pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     redirect_to edit_families_allergens_path if current_user.allergens.empty?
+    @products = Product.all
   end
 
   def test
