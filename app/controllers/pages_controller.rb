a3d27da578b515eaf19c781c1014a72bfaa769e7
Antonio Boavida <antonioruela@gmail.com>
2019-09-05 13:39:12 +0200

    css organization

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    redirect_to new_allergen_path if current_user.allergens.empty?
+    redirect_to edit_families_allergens_path if current_user.allergens.empty?
   end
 
   def test
