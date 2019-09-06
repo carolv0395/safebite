ca964d76d7b1d74b929c2a33deeac970d1180d6f
Pedro Pinheiro <pedrofgpinheiro@icloud.com>
2019-09-06 10:57:04 +0100

    images upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:photo, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :address])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  mount_uploader :avatar, AvatarUploader
+       
   has_many :orders
   has_many :allergens
 end
