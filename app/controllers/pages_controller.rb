19ef9bd2c53c2d7087e9d49c30ff1ceda1a9a582
Antonio Boavida <antonioruela@gmail.com>
2019-09-06 16:47:43 +0200

    Filter products in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,23 @@ class PagesController < ApplicationController
   def home
     redirect_to edit_families_allergens_path if current_user.allergens.empty?
     @products = Product.all
-    # show Products without Ingredients that belong to an AllergenFamily
-    @allergen_family = current_user.allergens_families[0].allergen_family.name if @allergen_family
+    if current_user.allergens_families[0]
+      @allergen_family = current_user.allergens_families[0].allergen_family.name
+      # show Products without Ingredients that belong to an AllergenFamily
+      allergen_family = AllergenFamily.find_by(name: @allergen_family)
+      unwanted_ingredients = Ingredient.where(allergen_family_id: allergen_family.id)
+      @filtered_products = []
+      is_product_inside_list = []
+      products_ingredients_lists = @products.map(&:ingredients)
+      products_ingredients_lists.each_with_index do |ingredient_list, index|
+        ingredient_list.each do |ingredient|
+          is_product_inside_list << "avoid" if unwanted_ingredients.include? ingredient
+        end
+        @filtered_products << @products[index] unless is_product_inside_list.include? "avoid"
+      end
+    else
+      @filtered_products = @products
+    end
   end
 
   def test
