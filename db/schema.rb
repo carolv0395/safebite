52c6d9172e8d374b0b68bd6ae9a09797df9e28d4
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-09-06 13:38:28 +0100

    brand to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_122514) do
+ActiveRecord::Schema.define(version: 2019_09_06_123704) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 2019_09_06_122514) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "brand"
     t.integer "category", default: 0
     t.string "volume"
     t.integer "discount"
@@ -82,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_09_06_122514) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "brand"
   end
 
   create_table "users", force: :cascade do |t|
