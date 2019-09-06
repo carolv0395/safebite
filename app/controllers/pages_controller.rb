0c317ce5e7cef274fb40353be1f733a50271a39e
Antonio Boavida <antonioruela@gmail.com>
2019-09-06 11:43:17 +0200

    filter products in homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   def home
     redirect_to edit_families_allergens_path if current_user.allergens.empty?
     @products = Product.all
+    # show Products without Ingredients that belong to an AllergenFamily
+    @allergen_family = current_user.allergens_families[0].allergen_family.name
   end
 
   def test
