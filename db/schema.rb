05e234e42f997b7f87f3469238fa573545f25544
Pedro Pinheiro <pedrofgpinheiro@icloud.com>
2019-09-06 15:35:19 +0100

    edit profile final

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_131428) do
-
+ActiveRecord::Schema.define(version: 2019_09_06_132955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,7 +72,6 @@ ActiveRecord::Schema.define(version: 2019_09_06_131428) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "volume"
     t.integer "discount"
     t.integer "stock"
     t.text "description"
@@ -83,6 +81,7 @@ ActiveRecord::Schema.define(version: 2019_09_06_131428) do
     t.integer "price_cents", default: 0, null: false
     t.string "brand"
     t.integer "category"
+    t.string "volume"
   end
 
   create_table "users", force: :cascade do |t|
@@ -94,12 +93,9 @@ ActiveRecord::Schema.define(version: 2019_09_06_131428) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "address"
-<<<<<<< HEAD
     t.string "avatar"
-=======
     t.string "first_name"
     t.string "last_name"
->>>>>>> master
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
