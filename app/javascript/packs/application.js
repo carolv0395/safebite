ca7c1c46a85121c588b3c504f66eadd3eee94e1d
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-09-10 17:07:17 +0100

    js imports

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,15 +1,12 @@
 import "bootstrap";
-<<<<<<< HEAD
+
 import { initImagePreview } from '../plugins/image-preview'
 import { sidebarCollapse } from '../plugins/sidebar'
+import { showSearchBar } from '../plugins/show-search-bar';
+
 
 initImagePreview();
 sidebarCollapse();
-=======
-import { initImagePreview } from '../plugins/image-preview';
-import { showSearchBar } from '../plugins/show-search-bar';
-
-initImagePreview();
 
 // Hide search bar
 const searchBar = document.querySelector('.search');
@@ -19,4 +16,3 @@ searchBar.classList.add('hide-search-bar', 0);
 if (document.querySelector('#show-search-bar')) {
   showSearchBar();
 };
->>>>>>> 175bd95e4df9afa690e7e17fbd39527baeab92b7
