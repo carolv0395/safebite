ede975ed993c9664309ebe9f79f79b88b3c60559
Carolina Vieira <carolina.vieira@novasbe.pt>
2019-09-12 14:42:39 +0100

    stripe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,11 +5,18 @@ import { sidebarCollapse } from '../plugins/sidebar'
 import { showSearchBar } from '../plugins/show-search-bar';
 import { flashDismiss } from '../plugins/flash-dismiss';
 import { removeFlash } from '../plugins/flash-dismiss';
+import { stripeFunction } from '../plugins/stripe'
 
 initImagePreview();
 sidebarCollapse();
 flashDismiss();
 removeFlash();
+
+const stripecheckoutpage = document.querySelector(".orders.shopping_cart");
+if (stripecheckoutpage){
+ stripeFunction();
+}
+
 
 // Hide search bar
 const searchBar = document.querySelector('.search');
